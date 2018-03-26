cfea4a0dd8dc4445f3eca18c5bf6c85ebd0edffe
maestrow <andrew.shmelev@gmail.com>
2018-03-26 16:35:53 +0300

    working pug rule

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
 const path = require('path');
-const glob = require('glob');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+var resolve = function (local) {
+  return path.join(__dirname, local);
+}
+
 module.exports = {
-  entry: () => new Promise((resolve) => 
-    resolve(glob.sync('src/*').reduce((acc, value) => {
-      acc[value] = './' + value;
-      return acc;
-    }, {}))
-  ),
-  output: {
-    filename: '[name].js',
-    publicPath: '/'
+  entry: {
+    page1: './src/index.pug',
+    page2: './src/index2.pug',
+  },
+  devServer: {
+    hot: true
   },
   module: {
     rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.pug$/, use: [
-        "file-loader?name=[name].html",
-        "extract-loader",
-        "html-loader",
-        "pug-html-loader"
-      ] },
-      { test: /\.png$/, use: ['file-loader'] },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      },
+      {
+        test: /\.pug$/,
+        use: [ 
+          "file-loader?name=[path][name].html",
+          "extract-loader",
+          "html-loader",
+          "pug-html-loader"
+        ]
+      }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(['dist/*']),
-    // new HtmlWebpackPlugin({
-    //   filename: 'file1.html',
-    //   template: './src/index.pug'
-    // }),
-    // new HtmlWebpackPlugin({
-    //   filename: 'file2.html',
-    //   template: './src/index2.pug'
-    // }),
+    new HtmlWebpackPlugin()
   ]
-};
+} 
